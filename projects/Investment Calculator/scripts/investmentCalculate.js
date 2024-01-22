7be172d878a46b7afef643f01918a7aab1d2f948
Naveen Raj <naveenraj.gl2002@gmail.com>
2024-01-22 20:27:23 +0530

    new commit

--- projects/Investment Calculator/scripts/investmentCalculate.js
+++ projects/Investment Calculator/scripts/investmentCalculate.js
@@ -23,7 +23,7 @@ export function calculateInvestmentResults({
 
 export const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'INR',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
 });
